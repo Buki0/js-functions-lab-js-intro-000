95d2bc201e55194c3ae5263a515a17b2c3dda6ea
interpreted-network-8638 <muratibuki@gmail.com>
2020-03-10 04:31:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function happyHolidays() {
+// write your code below!
+function happyHolidays(){
   return "Happy holidays!"
 }
 
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-
-function happyHolidayTo(holiday, name){
+function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
-}
 
 function holidayCountdown(holiday, days){
   return `It\'s ${days} days until ${holiday}!`
